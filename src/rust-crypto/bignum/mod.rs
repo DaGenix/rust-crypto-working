ac95936b38bc182e6c52e889d701a7eebc886caa
Palmer Cox <p@lmercox.com>
2015-01-03 18:36:46 -0500

    Working assoc types!

--- src/rust-crypto/bignum/mod.rs
+++ src/rust-crypto/bignum/mod.rs
@@ -9,35 +9,31 @@ use std::mem;
 use std::num::Int;
 use std::cmp;
 
-pub trait Digit<W>: Int {
-    fn as_word(self) -> W;
+pub trait Digit: Int {
+    type Word: WordOps<Self>;
+
+    fn as_word(self) -> Self::Word;
 }
 
-pub trait Word<D>: Int {
-    fn as_digit(self) -> D;
+pub trait WordOps<D>: Int {
     fn shift_digit_right(self) -> Self;
+    fn as_digit(self) -> D;
 }
 
-/*
-impl Digit<u64> for u32 {
-    fn as_word(self) -> u64 { self as u64 }
-    fn bits() -> uint { 32 }
-}
-*/
+// Helper functions to call WordOps methods since it looks
+// like a Rust bug is preventing method syntax from working.
+fn as_digit<D, W: WordOps<D>>(w: W) -> D { w.as_digit() }
+fn shift_digit_right<D, W: WordOps<D>>(w: W) -> W { w.shift_digit_right() }
 
-impl Digit<u32> for u16 {
+impl Digit for u16 {
+    type Word = u32;
+
     fn as_word(self) -> u32 { self as u32 }
 }
 
-/*
-impl Word<u32> for u64 {
-    fn as_digit(self) -> u32 { self as u32 }
-}
-*/
-
-impl Word<u16> for u32 {
-    fn as_digit(self) -> u16 { self as u16 }
+impl WordOps<u16> for u32 {
     fn shift_digit_right(self) -> u32 { self >> 16 }
+    fn as_digit(self) -> u16 { self as u16 }
 }
 
 pub trait Data<T>: Index<uint, T> + IndexMut<uint, T> + Slice<uint, [T]> + SliceMut<uint, [T]> {
@@ -160,15 +156,13 @@ macro_rules! bignum_data(
 
 bignum_data!(DataU16x100, u16, 100);
 
-
 pub struct Bignum<T> {
     pub pos: bool,
     pub data: T
 }
 
-impl <D, W, M> Bignum<M>
-        where D: Digit<W>, W: Word<D>, M: Data<D> {
-
+impl <D, M> Bignum<M>
+        where D: Digit, M: Data<D> {
     pub fn new() -> Bignum<M> {
         Bignum {
             pos: true,
@@ -177,8 +171,8 @@ impl <D, W, M> Bignum<M>
     }
 }
 
-pub fn clamp<D, W, M>(x: &mut Bignum<M>)
-        where D: Digit<W>, W: Word<D>, M: Data<D> {
+pub fn clamp<D, M>(x: &mut Bignum<M>)
+        where D: Digit, M: Data<D> {
     while x.data[].last().map_or(false, |&tmp| tmp == Int::zero()) {
         x.data.pop();
     }
@@ -188,7 +182,7 @@ pub fn clamp<D, W, M>(x: &mut Bignum<M>)
 }
 
 
-pub trait Ops<D, W, M>: Copy {
+pub trait Ops<D, M>: Copy {
     fn unsigned_add(&self, out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>);
     fn unsigned_sub(&self, out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>);
     fn muladd(&self, i: D, j: D, mut c0: D, mut c1: D, mut c2: D) -> (D, D, D);
@@ -225,18 +219,18 @@ impl <T: Copy, A: Iterator<T>, B: Iterator<T>> Iterator<(T, T)> for ZipWithDefau
     }
 }
 
-impl <D, W, M> Ops<D, W, M> for GenericOps
-        where D: Digit<W>, W: Word<D>, M: Data<D> {
+impl <D, M> Ops<D, M> for GenericOps
+        where D: Digit, M: Data<D> {
     fn unsigned_add(&self, out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>) {
         out.data.clear();
-        let mut t: W = Int::zero();
+        let mut t: <D as Digit>::Word = Int::zero();
         for (&tmpa, &tmpb) in zip_with_default(&Int::zero(), a.data[].iter(), b.data[].iter()) {
             t = t + tmpa.as_word() + tmpb.as_word();
-            out.data.push(t.as_digit());
-            t = t.shift_digit_right();
+            out.data.push(as_digit(t));
+            t = shift_digit_right(t);
         }
         if t != Int::zero() {
-            out.data.push(t.as_digit());
+            out.data.push(as_digit(t));
         }
         clamp(out);
     }
@@ -244,35 +238,35 @@ impl <D, W, M> Ops<D, W, M> for GenericOps
     /// out = a - b; abs(a) >= abs(b)
     fn unsigned_sub(&self, out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>) {
         out.data.clear();
-        let mut t: W = Int::zero();
+        let mut t: <D as Digit>::Word = Int::zero();
         let mut a_iter = a.data[].iter();
         for (&tmpa, &tmpb) in a_iter.by_ref().zip(b.data[].iter()) {
             t = tmpa.as_word() - tmpb.as_word() + t;
-            out.data.push(t.as_digit());
-            t = t.shift_digit_right();
+            out.data.push(as_digit(t));
+            t = shift_digit_right(t);
         }
         for &tmpa in a_iter {
             t = tmpa.as_word() - t;
-            out.data.push(t.as_digit());
-            t = t.shift_digit_right();
+            out.data.push(as_digit(t));
+            t = shift_digit_right(t);
         }
         clamp(out);
     }
 
     fn muladd(&self, i: D, j: D, mut c0: D, mut c1: D, mut c2: D) -> (D, D, D) {
-        let mut t: W;
+        let mut t: <D as Digit>::Word;
         t = c0.as_word() + i.as_word() * j.as_word();
-        c0 = t.as_digit();
-        t = c1.as_word() + t.shift_digit_right();
-        c1 = t.as_digit();
-        c2 = c2 + t.shift_digit_right().as_digit();
+        c0 = as_digit(t);
+        t = c1.as_word() + shift_digit_right(t);
+        c1 = as_digit(t);
+        c2 = c2 + as_digit(shift_digit_right(t));
         (c0, c1, c2)
     }
 }
 
 /// Unsigned comparison
-pub fn cmp_mag<D, W, M>(a: &Bignum<M>, b: &Bignum<M>) -> int
-        where D: Digit<W>, W: Word<D>, M: Data<D> {
+pub fn cmp_mag<D, M>(a: &Bignum<M>, b: &Bignum<M>) -> int
+        where D: Digit, M: Data<D> {
     if a.data.len() > b.data.len() {
         return 1;
     } else if a.data.len() < b.data.len() {
@@ -290,8 +284,8 @@ pub fn cmp_mag<D, W, M>(a: &Bignum<M>, b: &Bignum<M>) -> int
 }
 
 /// Signed comparison
-pub fn cmp<D, W, M>(a: &Bignum<M>, b: &Bignum<M>) -> int
-        where D: Digit<W>, W: Word<D>, M: Data<D> {
+pub fn cmp<D, M>(a: &Bignum<M>, b: &Bignum<M>) -> int
+        where D: Digit, M: Data<D> {
     if !a.pos && b.pos {
         return -1;
     } else if a.pos && b.pos {
@@ -307,8 +301,8 @@ pub fn cmp<D, W, M>(a: &Bignum<M>, b: &Bignum<M>) -> int
     }
 }
 
-pub fn add<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
-        where D: Digit<W>, W: Word<D>, M: Data<D>, O: Ops<D, W, M> {
+pub fn add<D, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
+        where D: Digit, M: Data<D>, O: Ops<D, M> {
     if a.pos == b.pos {
         out.pos = a.pos;
         ops.unsigned_add(out, a, b);
@@ -327,8 +321,8 @@ pub fn add<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O
     }
 }
 
-pub fn sub<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
-        where D: Digit<W>, W: Word<D>, M: Data<D>, O: Ops<D, W, M> {
+pub fn sub<D, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
+        where D: Digit, M: Data<D>, O: Ops<D, M> {
     // subtract a negative from a positive, OR
     // subtract a positive from a negative.
     // In either case, ADD their magnitudes,
@@ -355,8 +349,8 @@ pub fn sub<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O
     }
 }
 
-pub fn mul<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
-        where D: Digit<W>, W: Word<D>, M: Data<D>, O: Ops<D, W, M> {
+pub fn mul<D, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
+        where D: Digit, M: Data<D>, O: Ops<D, M> {
     let mut c0;
     let mut c1: D = Int::zero();
     let mut c2: D = Int::zero();
@@ -400,8 +394,8 @@ pub fn mul<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O
     clamp(out);
 }
 
-pub fn div<D, W, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
-        where D: Digit<W>, W: Word<D>, M: Data<D>, O: Ops<D, W, M> {
+pub fn div<D, M, O>(out: &mut Bignum<M>, a: &Bignum<M>, b: &Bignum<M>, ops: O)
+        where D: Digit, M: Data<D>, O: Ops<D, M> {
 
 }
 
